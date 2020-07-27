b153be7945c6a812d84b8a28972c1a32e8a6fcb9
Aderson Rangel <adersonrangel@gmail.com>
2020-07-27 16:29:38 -0500

    Agregar archivos de proyecto.

---
+++ Identity-Template/Context/DbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Identity_Template.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity_Template.Context
+{
+    public class TestDbContext : IdentityDbContext<IdentityUser>
+    {
+        /// <summary>
+        /// Class constructor
+        /// </summary>
+        public TestDbContext(DbContextOptions<TestDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
